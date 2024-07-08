5df7e17f88152e0c34e7dc22fa4d0b6181c7ed0c
Laponski <lapotap2007@gmail.com>
2024-07-08 11:46:18 +0200

    feat: login logout added, have to be in correct path
    BREAKING CHANGE:

--- app.py
+++ app.py
@@ -38,8 +38,37 @@ PLACEHOLDER_CODE = ""
 DEFAULT_STYLE = "github-dark"
 NO_CODE_FALLBACK = "# No Code Entered"
 
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("code", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
 @app.route("/", methods=["GET"])
 def code():
+
+    return render_template("prova.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
     if session.get("code") is None:
         session["code"] = PLACEHOLDER_CODE
@@ -133,6 +162,6 @@ def load_github_file():
     return redirect(url_for("code"))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+        app.run(host="0.0.0.0", port=env.get("PORT", 3000))
 
 
